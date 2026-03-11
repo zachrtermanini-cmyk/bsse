0c4daa636a2b7027f53ab48417ae76a87536a44c
zachrtermanini-cmyk <zachrtermanini@gmail.com>
2026-03-11 15:16:56 -0400

    Update playerGear.js

--- playerGear.js
+++ playerGear.js
@@ -27,7 +27,7 @@ window.playerGear={
                 cylinder(x,y+1.25*0.5,z,0.16,0.15,10,0.3,0.3,0.3,90,0,0)
             },
             desc:'When planted in the ground, causes nearby flowers to regrow faster!<br><br>Count: 1<br>Diameter: 10<br>Power: 20<br>Rate: 4s<br><br>Press "R" to place a sprinkler.',
-            cost:['1111111 honey'],
+            cost:['1 honey'],
         },
 
         silverSoakers:{
